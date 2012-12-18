6db2ed93189cd39c65f40d14fcbb0ed82964963e
murvinlai <murvinlai@gmail.com>
2012-12-18 15:23:34 -0800

    Init

---
+++ server/httpServer.js
@@ -0,0 +1,20 @@
+var express = require('express');
+
+/* handle uncaught exception */
+process.on('uncaughtException', function (ex) {
+	console.error(ex);
+	console.log("UncaughtException: " + ex);
+});
+
+// create server
+var app = express();
+
+app.set('title', 'Music Application');
+app.use(express.logger());
+app.use(express.bodyParser());
+app.use(express.static(__dirname + '/public'));
+app.use(app.router);
+
+// Listen to port 
+app.listen(3000);
+
