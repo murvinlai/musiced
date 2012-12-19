100945db0841be3e11f1026334bc77d61ef9790f
murvinlai <murvinlai@gmail.com>
2012-12-18 17:32:29 -0800

    2nd Version.. refine.

--- server/public/js/music.js
+++ server/public/js/music.js
@@ -27,8 +27,27 @@ var MM = {
 	
 	init: function() {
 		if ( $('#'+MM.settings.hiddenDom).html() == undefined) {
-			$('body').append("<div id='"+MM.settings.hiddenDom+"' class='Hidden'></div>")
+			$('body').append("<div id='"+MM.settings.hiddenDom+"' class='Hidden'></div>");
 		}
+		MM.setInitImage();
+	},
+	
+	setInitImage: function(){
+		$('#'+MM.settings.playDom).append('<div id="initImage"></div>');
+	},
+	
+	setLoadingImage: function(){
+		$('#'+MM.settings.playDom).append('<div id="loadingImage"></div>');
+	},
+	
+	playFromSlider: function(start, end){
+		MM.currIndex = -1;
+		if (MM.checkInt != false) {
+			clearInterval(MM.checkInt);
+		}
+		MM.setCustList(start, end);
+		MM.playCustList();
+		MM.checkInt = setInterval( MM.checkEnded, 60);
 	},
 	
 	playCust:	function () {
@@ -50,7 +69,7 @@ var MM = {
 		}				
 	},
 	
-	setCustList: function() {
+	setCustList: function(start, end) {
 		// clean up dom
 		if (MM.current != false) {
 			//var cDom = document.getElementById(MM.current.domId);
@@ -59,35 +78,54 @@ var MM = {
 			// remove from parent
 			$('#'+MM.current.domId).remove();
 		}
+		$('#'+MM.settings.playDom).empty();
+		
 		MM.current = false;
 		// clean up hidden
 		$('#'+MM.settings.hiddenDom).empty();
 		MM.custList = [];
 		
 		// clean up title
-		$('#title').html('');
+		$('#title').html('Loading...');
 		
-		for (var i=1; i<=5; i++) {
-			if ( document.getElementById(MM.settings.checkListPrefix+i).checked) {
-				var temp = {pid:i, domId: MM.settings.domPrefix+i, length:8.3, start:(i-1)*8.3, end:(i-1)*8.3+8.3};
-				MM.custList.push(temp);
-				// now create the object ,in hiddenDOm, and then stop playing it.
-				var hiddenDom = document.getElementById(MM.settings.hiddenDom);
-				var musicObj = document.createElement('video');
-				musicObj.id = temp.domId;
-				musicObj.src = '/video/'+temp.pid + ".mp4";
-				musicObj.height = '500';
-				musicObj.controls = 'controls';
-				musicObj.pause();
-				hiddenDom.appendChild(musicObj);
-				
+		if (start == undefined && end == undefined) {
+			var hiddenDom = document.getElementById(MM.settings.hiddenDom);
+			for (var i=1; i<=5; i++) {
+				if ( document.getElementById(MM.settings.checkListPrefix+i).checked) {
+					MM.setCustObj(hiddenDom, i);
+				}
+			} 
+		} else if (start != undefined && end != undefined) {
+			var startIndex = parseInt(start, 10);
+			var endIndex = parseInt(end, 10);
+			var hiddenDom = document.getElementById(MM.settings.hiddenDom);
+			
+			for (var i=startIndex; i<= endIndex; i++) {
+				MM.setCustObj(hiddenDom, i);
 			}
-		} 
+			
+		}
 	},	
+	
+	setCustObj: function(hiddenDom, i) {
+		var temp = {pid:i, domId: MM.settings.domPrefix+i, length:8.3, start:(i-1)*8.3, end:(i-1)*8.3+8.3};
+		MM.custList.push(temp);
+		// now create the object ,in hiddenDOm, and then stop playing it.
+		var hiddenDom = document.getElementById(MM.settings.hiddenDom);
+		var musicObj = document.createElement('video');
+		musicObj.id = temp.domId;
+		musicObj.src = '/video/'+temp.pid + ".mp4";
+		musicObj.height = '500';
+		musicObj.controls = 'controls';
+		musicObj.pause();
+		hiddenDom.appendChild(musicObj);
+	},
 	
 	playCustList: function() {
 		if (MM.custList.length > 0) {
-			MM.playNext();
+			MM.setLoadingImage();
+			
+			setTimeout(MM.playNext, 500);
 		}
 	},
 	
@@ -109,6 +147,7 @@ var MM = {
 			var currentDom = document.getElementById(MM.current.domId);
 			hiddenDom.appendChild(currentDom);
 		}
+		$('#'+MM.settings.playDom).empty();
 		
 		// now set the new current
 		MM.current = MM.custList[MM.currIndex];
